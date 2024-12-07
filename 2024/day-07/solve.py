c1d4fb407487ddcfe35933489eb2b2ad06713b37
Tilman <tilmanburghoff@posteo.de>
2024-12-07 14:44:09 +0100

    day 7 part 1

---
+++ 2024/day-07/solve.py
@@ -0,0 +1,24 @@
+from operator import add, mul
+
+def is_solvalble(terms, solution):
+    partial_solution = {int(terms[0])}
+    for term in terms[1:]:
+        term = int(term)
+        new_solution = set()
+        while partial_solution:
+            sol = partial_solution.pop()
+            for op in (add, mul):
+                new_solution.add(op(sol, term))
+        partial_solution = new_solution
+    return solution in partial_solution
+
+
+with open("input.txt") as f:
+    output = 0
+    for line in f.readlines():
+        solution, terms = line.split(": ")
+        solution = int(solution)
+        if is_solvalble(terms.split(), solution):
+            output += solution
+
+print(output)
